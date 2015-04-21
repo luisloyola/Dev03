dab289aae37b6fb3054f6ea67bea850303d8f907
Matiasqj <matias.quinteros@usach.cl>
2015-04-20 23:46:27 -0300

    Arreglado configuración del js y images, Linkeo de los elementos dialogicos y modal para mostrar las reuniones

--- l-pink/config/initializers/assets.rb
+++ l-pink/config/initializers/assets.rb
@@ -1,8 +1,14 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
 
+Rails.application.config.assets.precompile += %w( materialize.min.css ) 
+Rails.application.config.assets.precompile += %w( bootstrap-wysihtml5.css ) 
+Rails.application.config.assets.precompile += %w( paletaColores.css ) 
+Rails.application.config.assets.precompile += %w( font-awesome.min.css ) 
+Rails.application.config.assets.precompile += %w( materialize.min.js ) 
+Rails.application.config.assets.precompile += %w( wysihtml-toolbar.min.js ) 
+Rails.application.config.assets.precompile += %w( advanced_and_extended.js )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
